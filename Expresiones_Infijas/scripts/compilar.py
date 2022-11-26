aa6e58c133fb23f246f9f8b329e716829e2c22e7
Darío Quiñones <daqucr2@gmail.com>
2022-11-26 15:55:06 -0600

    Compilación más permisiva

--- Expresiones_Infijas/scripts/compilar.py
+++ Expresiones_Infijas/scripts/compilar.py
@@ -9,29 +9,42 @@
 
 import os
 
-# # sistema operativo
-# so = os.uname().sysname
-
-# Compilar el codigo fuente
-compilador = "gcc"
-# Opciones extra para el compilador
-flags = "-Wall -Wextra -Werror -pedantic -std=c99 -g"
-
 # Dependiendo si se utilizan librerías de la pila dinámica o estática
+# Colocado aquí para que sea más fácil cambiarlo
 librerias = "pilas/pila-dinamica.c"
-# librerias = "pilas/pila-estatica.c"
-output = "-o bin/Expresiones_Infijas"
+# librerias = "pilas/pila_estatica.c"
 
-# Si la carpeta bin no existe, crearla
-if not os.path.exists("bin"):
-    os.mkdir("bin")
+def main():
+    # # sistema operativo
+    # so = os.uname().sysname
 
-# Asegurarse que el gcc se encuentra instalado
-if not os.system("which gcc"):
     # Compilar el codigo fuente
-    print("Compilando el codigo fuente...")
-    print(f"{compilador} {flags} main.c {librerias} {output}")
-    os.system(f"{compilador} {flags} {output} main.c {librerias}")
-else:
-    print("El compilador gcc no se encuentra instalado")
+    compilador = "gcc"
 
+    # Opciones extra para el compilador
+    # -std=c99: para que el compilador use el estandar C99
+    # -Wall: para que el compilador habilite más warnings
+    # -pedantic: para que el compilador muestre warnings para adherirse al estandar ansi C
+    # -g: para que el compilador genere informacion para el debugger
+    # -lm: para que el compilador linkee con la libreria matematica
+    flags = "-std=c99 -Wall -Wextra -pedantic -g -lm"
+
+    # librerias = "pilas/pila-estatica.c"
+    output = "-o bin/Expresiones_Infijas"
+
+    # Si la carpeta bin no existe, crearla
+    if not os.path.exists("bin"):
+        os.mkdir("bin")
+
+    # Asegurarse que el gcc se encuentra instalado
+    if not os.system("which gcc"):
+        # Compilar el codigo fuente
+        print("Compilando el codigo fuente...")
+        print(f"{compilador} {flags} {output} main.c {librerias}")
+        os.system(f"{compilador} {flags} {output} main.c {librerias}")
+    else:
+        print("El compilador gcc no se encuentra instalado")
+
+# Si este script es ejecutado, ejecutar la funcion main
+if __name__ == "__main__":
+    main()
