862a245bd5047836fd6b82d00acc15ef7f1aa586
Darío Quiñones <daqucr2@gmail.com>
2022-12-12 14:46:18 -0600

    Renombrados archivos

--- Simulaciones_con_el_TAD_cola/lib/presentacionLin.c
+++ Simulaciones_con_el_TAD_cola/lib/presentacionLin.c
@@ -8,7 +8,8 @@ Compilación de la libreria: Linux (gcc -c presentacionLin.c)
 //LIBRERIAS
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  		//Libreria no ANSI C
+#include <unistd.h>  		//Libreria no ANSI C/
+// #include <time.h>
 #include"presentacion.h"
 
 //DEFINICIÓN DE FUNCIONES
@@ -18,12 +19,17 @@ void MoverCursor( int x, int y )
 {
 	printf("%c[%d;%df",0x1B,y,x);
 	return;
-};
+}
 
 //Función para esperar un tiempo en milisegundos, simulación de la función delay() que se tenia en borland 3.0 en la libreria conio.h
 void EsperarMiliSeg(int t)
 {
-	usleep(t*1000);
+	// usleep fue reemplazada por nanosleep en POSIX.1-2001
+	usleep(t*1000);		//Función no ANSI C
+	// struct timespec tim, tim2;
+	// tim.tv_sec = 0;
+	// tim.tv_nsec = t*1000000L;
+	// nanosleep(&tim , &tim2);
 	return;
 }
 
